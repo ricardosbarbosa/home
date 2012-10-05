d151054783ba78b0c63622fc57efbcf23bb79bfe
Ricardo Barbosa <rbrico@gmail.com>
2012-10-05 10:10:11 -0300

    private message

---
+++ app/models/message.rb
@@ -0,0 +1,12 @@
+class Message < ActiveRecord::Base
+
+  is_private_message
+  
+  attr_accessible :to, :subject, :body
+  
+  # The :to accessor is used by the scaffolding,
+  # uncomment it if using it or you can remove it if not
+  attr_accessor :to, :subject, :body
+
+  
+end
